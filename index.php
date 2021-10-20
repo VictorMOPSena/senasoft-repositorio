a4fe74b4e684abfa0cf38e82589439ebd059174a
andres tellez <andr3sgamer@gmail.com>
2021-10-19 19:04:25 -0500

    validacion de ver personas
    se vieron personas

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/inicio_sesion.css">
     <title>Inicio de sesion</title>
 </head>
 <body>
@@ -12,8 +13,9 @@
         <div class="container_form"> 
             <form action="" method="POST" class="form_login">
                 <h1>INICIO DE SESION</h1><br><br>
-            <i class="fas fa-user-circle"><input type="text" class="input_text"></i><br><br>
-            <i class="fas fa-key"><input type="password" class="input_text"></i><br><br>
+            <i class="fas fa-user-circle"><input type="text" class="input_text" placeholder="Usuario"></i><br><br>
+            <i class="fas fa-key"><input type="password" class="input_text" placeholder="Contraseña"></i><br><br>
+            <input type="submit" class="btn_input" value="Ingresar"><br><br>
             </form>
 
         </div>
