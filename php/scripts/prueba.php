78bac8d1fdf128ce9a3f63d4f0dc578101a8942f
VictorMOPSena <92410221+VictorMOPSena@users.noreply.github.com>
2021-10-20 16:50:20 -0500

    Cambios

--- php/scripts/prueba.php
+++ php/scripts/prueba.php
@@ -4,6 +4,13 @@
     require_once '../classes/persona.class.php';
     require_once "../codigos-mensajes.php";
 
+
+    $fecha = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+    $tomorrow  = date("d-M-Y", $fecha)."-".date("l", $fecha);
+    echo $tomorrow;
+
+    "select Date,TotalAllowance from Calculation where EmployeeId=1 and Date >= '2011/02/25' and Date < '2011/02/28'";
+
     // $empleado = new Empleado();
     // $respuesta = $empleado->AgregarEmpleado("fsss","e","easd@g.com","aasa");
     // echo $codigosMensajes[$respuesta["respuesta"]];
@@ -29,27 +36,27 @@
     // }
 
 
-    $personaClass = new Persona();
-    $respuesta = $personaClass->CedulaExistente(1126458612);
+    // $personaClass = new Persona();
+    // $respuesta = $personaClass->CedulaExistente(1126458612);
 
-    if($respuesta["estado"]){
-    $resultados=$respuesta["stmt"]->fetchAll(PDO::FETCH_OBJ);
-    foreach($resultados as $resultado){
-        echo $resultado->idPersona." - ";
-        echo $resultado->cedulaPersona." - ";
-        echo $resultado->nombresPersona." - ";
-        echo $resultado->apellidosPersona." - ";
-        echo $resultado->celularPersona." - ";
-        echo $resultado->correoPersona." - ";
-        echo $resultado->direccionPersona." - ";
-        echo $resultado->idEspecialidadPersona." - ";
-        echo $resultado->nombreEspecialidad." - ";
-        echo "<br>";
+    // if($respuesta["estado"]){
+    // $resultados=$respuesta["stmt"]->fetchAll(PDO::FETCH_OBJ);
+    // foreach($resultados as $resultado){
+    //     echo $resultado->idPersona." - ";
+    //     echo $resultado->cedulaPersona." - ";
+    //     echo $resultado->nombresPersona." - ";
+    //     echo $resultado->apellidosPersona." - ";
+    //     echo $resultado->celularPersona." - ";
+    //     echo $resultado->correoPersona." - ";
+    //     echo $resultado->direccionPersona." - ";
+    //     echo $resultado->idEspecialidadPersona." - ";
+    //     echo $resultado->nombreEspecialidad." - ";
+    //     echo "<br>";
 
-    }}else{
-        echo $codigosMensajes[$respuesta["respuesta"]];
+    // }}else{
+    //     echo $codigosMensajes[$respuesta["respuesta"]];
     
-    }
+    // }
 
     // if($respuesta["estado"]){
     //     $resultados=$respuesta["stmt"]->fetchAll(PDO::FETCH_OBJ);
