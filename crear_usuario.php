f5ce2cd773b0da6c5c4041c05489ca6cd2f92ea9
andr3sgamer <69479885+andr3sgamer@users.noreply.github.com>
2021-10-20 16:23:25 -0500

    valdiacion de usuarios

--- crear_usuario.php
+++ crear_usuario.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/interfaces/agregar_personal.css">
+    <link rel="stylesheet" href="./assets/css/interfaces/agregar_usuario.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
-    <title>Agregar Persona</title>
+    <title>Document</title>
 </head>
 <body>
 
@@ -30,46 +30,42 @@
         </div>
     </div>
 
-
-
     <div class="container_principal">
         <div class="container_form">
-        <form action="./php/scripts/persona/agregar-persona.script.php" method="POST" class="container_agregar_persona">
-            <h1>Datos para registrar nueva persona</h1>
-            <center>
-            <input type="text" class="input_text" name="cedula" placeholder="Documento"><br>
-            <input type="text" class="input_text" name="nombre" placeholder="Nombres"><br>
-            <input type="text" class="input_text" name="apellido" placeholder="Apllidos"><br>
-            <input type="text" class="input_text" name="celular" placeholder="Celular"><br>
-            <input type="email" class="input_text" name="correo" placeholder="Correo Electronico"><br>
-            <input type="text" class="input_text" name="direccion" placeholder="Direcion"><br>
-            <input list="especialidades" class="input_text" name="especialidad" placeholder="Especialidad"><br>
-            
-            <datalist id="especialidades">
-                <?php
 
-                    require_once "./php/classes/conexion.class.php";
-                    require_once "./php/classes/especialidad.class.php";
-                
-                    $especialidadClass = new Especialidad();
+        <form action="./php/scripts/persona/agregar-persona.script.php" method="POST" class="container_crear_usuario">
+            <h1>CREAR NUEVO USUARIO</h1>
+            <center>
+            <input type="text" class="input_text" name="usuario" placeholder="Crear Usuario"><br>
+            <input type="text" class="input_text" name="contraseña" placeholder="Crear Contraseñas"><br>
+            <input list="cedulas" class="input_text" name="docuementos" placeholder="Documentos"><br>
+            <datalist id="cedulas">
+                <?php
+                 require_once "./php/classes/conexion.class.php";
+                 require_once "./php/classes/persona.class.php";
+
+                 $personaClass = new Persona();
                     
-                    $respuesta=$especialidadClass->ObtenerEspecialidades();
+                    $respuesta=$personaClass->ObtenerPersonas();
                     
                     if($respuesta["estado"]){
                         $resultados=$respuesta["stmt"]->fetchAll(PDO::FETCH_OBJ);
                             foreach($resultados as $resultado){
                 ?>
-                                    <option value="<?php echo $resultado->nombreEspecialidad;?>"></option>
+                    <option value="<?php echo $resultado->cedulaPersona;?>"></option>
                 <?php                       
                             }     
-                    }
-
+                    }        
                 ?>
             </datalist>
-            <input type="submit" class="btn_input" value="Agregar">
+            
+    
+            <input type="submit" class="btn_input" value="Crear">
             </center>
         </form>
+
         </div>
     </div>
+
 </body>
 </html>
