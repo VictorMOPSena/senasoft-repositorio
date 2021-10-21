ffe87e61e39da57c034659b2cfed76362336bce5
andres tellez <andr3sgamer@gmail.com>
2021-10-20 19:21:00 -0500

    cerrar sesion

--- index_jefe.php
+++ index_jefe.php
@@ -41,6 +41,9 @@
             <div class="container_img_profile">
                 <a href=""><img src="./assets/img/img_profile.jpg" alt=""></a>
             </div>
+            <div class="container_cerrar_sesion">
+                <a href="./php/scripts/sesion/cerrar-sesion.script.php"><i class="fas fa-user"> Cerrar sesion</i></a>
+            </div>
         </div>
     </div>
 
