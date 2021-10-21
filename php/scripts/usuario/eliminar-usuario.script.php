484d6a96b251d6db62d774b1550cf21b4d58eb00
VictorMOPSena <92410221+VictorMOPSena@users.noreply.github.com>
2021-10-21 11:42:14 -0500

    Error Eliminar usuario -cronograma solucionado

--- php/scripts/usuario/eliminar-usuario.script.php
+++ php/scripts/usuario/eliminar-usuario.script.php
@@ -8,11 +8,17 @@
     $idInput = $_GET['id'];
 
     $cronogramaClass = new Cronograma();
-    $respuesta = $cronogramaClass->EliminarUsuario($idInput);
+    $respuesta = $cronogramaClass->SetUsuarioNoExistente($idInput);
 
-    $usuarioClass = new Usuario();
-    $respuesta = $usuarioClass->EliminarUsuario($idInput);
+    if($respuesta['estado']){
+        $usuarioClass = new Usuario();
+        $respuesta = $usuarioClass->EliminarUsuario($idInput);
+        
+    }
+
     $mensaje = $respuesta["respuesta"];
     header ("location: ../../../ver_usuarios.php?msn=$mensaje");
+
+    // echo $codigosMensajes[$respuesta["respuesta"]]."<br>";
 
 ?>
