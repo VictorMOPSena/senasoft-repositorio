42d9cdf5c22fe75f87e4cc1513284f74886809ad
andr3sgamer <69479885+andr3sgamer@users.noreply.github.com>
2021-10-21 12:40:37 -0500

    validar actualizar persona

--- actualizar_personal.php
+++ actualizar_personal.php
@@ -20,7 +20,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/interfaces/ver_personal.css">
+    <link rel="stylesheet" href="./assets/css/interfaces/actualizar_personal.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
@@ -39,7 +39,7 @@
                 Bienvenido <?php echo $_SESSION['nombreUsuarioSenasoft']?>
             </div>
             <div class="container_img_profile">
-                <a href="actualizar_usuario.php"><img src="./assets/img/img_profile.jpg" alt=""></a>
+                <a href="actualizar_perfil.php"><img src="./assets/img/img_profile.jpg" alt=""></a>
             </div>
             <div class="container_cerrar_sesion">
                 <a href="./php/scripts/sesion/cerrar-sesion.script.php"><i class="fas fa-user"> Cerrar sesion</i></a>
@@ -47,30 +47,36 @@
         </div>
     </div>
 
-    <div class="container_table">
+    <div class="container_principal">
+        <div class="container_form">
             <?php
-            require_once "./php/classes/conexion.class.php";
-            require_once "./php/classes/persona.class.php";
-            require_once "./php/codigos-mensajes.php";
+                require_once "./php/classes/conexion.class.php";
+                require_once "./php/classes/persona.class.php";
+                require_once "./php/codigos-mensajes.php";
             
-            $personaClass = new Persona();
-            $respuesta=$personaClass->PersonaExistente($_GET['id']);
+                $personaClass = new Persona();
+                $respuesta=$personaClass->PersonaExistente($_GET['id']);
 
-            if($respuesta["estado"]){
-                 $resultados=$respuesta["stmt"]->fetchAll(PDO::FETCH_OBJ);
-                     foreach($resultados as $resultado){
-                         ?>
-                        <form action="./php/scripts/persona/actualizar-persona.script.php" method="POST">
-                            <input type="hidden" class="input_text" name="id" value="<?php echo $resultado->idPersona;?>">  
-                            <input type="text" class="input_text" name="cedula" value="<?php echo $resultado->cedulaPersona;?>">
-                            <input type="text" class="input_text" name="nombre" value="<?php echo $resultado->nombresPersona;?>">
-                            <input type="text" class="input_text" name="apellido" value="<?php echo $resultado->apellidosPersona;?>">
-                            <input type="text" class="input_text" name="celular" value="<?php echo $resultado->celularPersona;?>">
-                            <input type="email" class="input_text" name="correo" value="<?php echo $resultado->correoPersona;?>">
-                            <input type="text" class="input_text" name="direccion" value="<?php echo $resultado->direccionPersona;?>">
-                            <input list="especialidades" class="input_text" name="especialidad" placeholder="Especialidad"><br>
+                if($respuesta["estado"]){
+                    $resultados=$respuesta["stmt"]->fetchAll(PDO::FETCH_OBJ);
+                        foreach($resultados as $resultado){
+                            ?>
+
+                <form action="./php/scripts/persona/actualizar-persona.script.php" method="POST" class="container_crear_usuario">
+                    <h1>ACTUALIZAR PERSONAS</h1>
+                    <center>
+                        
+                    <input type="hidden" class="input_text" name="id" value="<?php echo $resultado->idPersona;?>"> <br> 
+                    <input type="text" class="input_text" name="cedula" value="<?php echo $resultado->cedulaPersona;?>"><br>
+                    <input type="text" class="input_text" name="nombre" value="<?php echo $resultado->nombresPersona;?>"><br>
+                    <input type="text" class="input_text" name="apellido" value="<?php echo $resultado->apellidosPersona;?>"><br>
+                    <input type="text" class="input_text" name="celular" value="<?php echo $resultado->celularPersona;?>"><br>
+                    <input type="email" class="input_text" name="correo" value="<?php echo $resultado->correoPersona;?>"><br>
+                    <input type="text" class="input_text" name="direccion" value="<?php echo $resultado->direccionPersona;?>"><br>
+                    <input list="especialidades" class="input_text" name="especialidad" placeholder="Especialidad"><br>
+       
             
-                            <datalist id="especialidades">
+                    <datalist id="especialidades">
                             <?php
 
                                 require_once "./php/classes/conexion.class.php";
@@ -93,11 +99,14 @@
                                 }
 
                             ?>
-                            </datalist>
-                            <input type="submit" value="Acualizar"></a></td>
+                        </datalist>
+                        <input type="submit" class="btn_input" value="Actualizar">
 
-                            </form>
-                            <?php
+                            </center>
+            
+                </form>
+
+        <?php
                             }
                         
                             }else{
@@ -106,7 +115,9 @@
                             }
 
                             ?>
-            
+
+        </div>
     </div>
+
 </body>
 </html>
