ca544328d3a73669c96fd216ea934a27bca8a57c
andr3sgamer <69479885+andr3sgamer@users.noreply.github.com>
2021-10-21 11:38:51 -0500

    actualizar usuario

--- index_empleados.php
+++ index_empleados.php
@@ -1,13 +1,15 @@
 <?php
-/*
+
 session_start();
-
-$usuario = $_SESSION[''];
-
-if($usuario == null || $usuario == ""){
-    echo "no tiene permiso";    
-}*/
-
+/*
+if(isset($_SESSION['idRolUsuarioSenasoft'])){
+    if($_SESSION['idRolUsuarioSenasoft'] != 1){
+        header ("location: index.php");
+    }
+}else{
+    header ("location: index.php");
+}
+*/
 ?>  
 
 <!DOCTYPE html>
@@ -17,47 +19,71 @@ if($usuario == null || $usuario == ""){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/interfaces/index_empleados.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
     <title>Index empleados</title>
 </head>
 <body>
-    <div class="container_menu">
+<div class="container_menu">
         <div class="container_img_logo">
-            imagen del logo
+            <img src="./assets/img/Logo.png" alt="">
         </div>
         <div class="container_perfil">
-            <div class="">
-                nombre usuario
+            <div class="container_name_user">
+                Bienvenido <?php echo $_SESSION['nombreUsuarioSenasoft']?>
             </div>
             <div class="container_img_profile">
-                imagen de perfil del usuario
+                <a href="actualizar_usuario.php"><img src="./assets/img/img_profile.jpg" alt=""></a>
             </div>
             <div class="container_cerrar_sesion">
                 <a href="./php/scripts/sesion/cerrar-sesion.script.php"><i class="fas fa-user"> Cerrar sesion</i></a>
             </div>
         </div>
-
     </div>
 
-    <div class="container_options">
-        <div class="container_turno">
-            <div class="icono_turno">
-                <i class="fas fa-file-signature"></i>
-            </div>
-            <div class="informacion">
-                <h1>presione para seleccionar turnos de la semana</h1>
-            </div>
-        </div>
-        
-        <div class="ver_turnos">
-        <div class="icono_calendario">
-            <i class="fas fa-calendar"></i>
-            </div>
-            <div class="informacion">
-                <h1>presione ver los turnos de la semana</h1>
-                <a href="./php/scripts/sesion/cerrar-sesion.script.php"><i class="fas fa-user"> Cerrar sesion</i></a>
-            </div>
-        </div>
+    <div class="container_principal">
+        <div class="container_tabla_turnos">
+            <table class="tabla_turnos">
+                <tr class="titulos_tabla">
+                    <th>HORARIOS</th>
+                    <th>LUNES</th>
+                    <th>MARTES</th>
+                    <th>MIERCOLES</th>
+                    <th>JUEVES</th>
+                    <th>VIERNES</th>
+                    <th>SABADO</th>
+                    <th>DOMINGO</th>
+                </tr>
 
+                <tr class="info_turnos">
+                    <td>6:00</td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                </tr>
+
+                <tr class="info_turnos">
+                    <td>18:00</td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                    <td><input type="submit" class="btn_turno"></td>
+                </tr>
+            </table>
+        </div>
     </div>
     
 </body>
