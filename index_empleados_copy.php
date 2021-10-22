1f55505818501ca3c1dcee7c90625b36825e1f98
andr3sgamer <69479885+andr3sgamer@users.noreply.github.com>
2021-10-22 10:38:02 -0500

    diseño turno

--- index_empleados_copy.php
+++ index_empleados_copy.php
@@ -92,12 +92,10 @@ if(isset($_SESSION['idRolUsuarioSenasoft'])){
     <div id="ventana-emergente">
         <div class="empleados-boton">
             <div id="nombres-empleados">
-                <!-- <div">
-                    <p>nombre</p>
-                </div> -->
+                    <p id="titulo_empleados">Empleados con este turno:</p>
             </div>
-            <button id="boton-turno">Accion</button>
-            <button id="boton-cerrar">Cerrar</button>
+                <button id="boton-turno">Accion</button>
+                <button id="boton-cerrar">Cerrar</button>
         </div>
     </div>
     <script src="./assets/js/escoger-turno.js"></script>
