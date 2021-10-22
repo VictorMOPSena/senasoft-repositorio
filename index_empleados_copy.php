dc9304369adcaa75a15a554f66505e5b901bfc79
VictorMOPSena <92410221+VictorMOPSena@users.noreply.github.com>
2021-10-22 08:23:38 -0500

    copias

--- index_empleados_copy.php
+++ index_empleados_copy.php
@@ -1,17 +1,16 @@
 <?php
 
-    session_start();
+session_start();
 
-    if(isset($_SESSION['idRolUsuarioSenasoft'])){
-        if($_SESSION['idRolUsuarioSenasoft'] != 1){
-            header ("location: index.php");
-        }
-    }else{
+if(isset($_SESSION['idRolUsuarioSenasoft'])){
+    if($_SESSION['idRolUsuarioSenasoft'] != 2){
         header ("location: index.php");
     }
+}else{
+    header ("location: index.php");
+}
 
-?>
-
+?>  
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,45 +18,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/css/tablas_turnos.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/tablas_turnos_copy.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
-    <title>Index jefe</title>
-    <title>Tabla estandar</title>
+    <title>Index empleados</title>
 </head>
 <body>
 <div class="container_menu">
-        <input id="idUsuario" type="hidden" value="<?php echo $_SESSION['idUsuarioSenasoft']?>">
+    <input id="idUsuario" type="hidden" value="<?php echo $_SESSION['idUsuarioSenasoft']?>">
         <div class="container_img_logo">
-            <img src="../assets/img/Logo.png" alt="">
+            <img src="./assets/img/Logo.png" alt="">
         </div>
         <div class="container_perfil">
             <div class="container_name_user">
                 Bienvenido <?php echo $_SESSION['nombreUsuarioSenasoft']?>
             </div>
             <div class="container_img_profile">
-                <img src="../assets/img/img_profile.jpg" alt="">
+                <img src="./assets/img/img_profile.jpg" alt="img_perfil">
             </div>
             <div class="container_cerrar_sesion">
-                <a href="../php/scripts/sesion/cerrar-sesion.script.php"><i class="fas fa-user"> Cerrar sesion</i></a>
-            </div>
-            <div class="container_regresar">
-                <a href="../index_jefe.php"><i class="fas fa-undo-alt"> Regresar</i></a>
+                <a href="./php/scripts/sesion/cerrar-sesion.script.php"><i class="fas fa-user"> Cerrar sesion</i></a>
             </div>
         </div>
     </div>
+
     <center>
-        <h1 id="h1FechaCronograma">Hola</h1>
+        <h1 id="h1FechaCronograma">Fecha</h1>
     </center>
-    
+
     <div class="container_principal">
         <div class="container_tabla_turnos">
             <table class="tabla_turnos">
@@ -74,29 +68,28 @@
 
                 <tr class="info_turnos">
                     <td>6:00</td>
-                    <td><div dia="0" horario="1" class="btn_turno"></td>
-                    <td><div dia="1" horario="1" class="btn_turno"></td>
-                    <td><div dia="2" horario="1" class="btn_turno"></td>
-                    <td><div dia="3" horario="1" class="btn_turno"></td>
-                    <td><div dia="4" horario="1" class="btn_turno"></td>
-                    <td><div dia="5" horario="1" class="btn_turno"></td>
-                    <td><div dia="6" horario="1" class="btn_turno"></td>
+                    <td><div dia="0" horario="1" class="turno_no_escogido"></td>
+                    <td><div dia="1" horario="1" class="turno_no_escogido"></td>
+                    <td><div dia="2" horario="1" class="turno_no_escogido"></td>
+                    <td><div dia="3" horario="1" class="turno_no_escogido"></td>
+                    <td><div dia="4" horario="1" class="turno_no_escogido"></td>
+                    <td><div dia="5" horario="1" class="turno_no_escogido"></td>
+                    <td><div dia="6" horario="1" class="turno_no_escogido"></td>
                 </tr>
 
                 <tr class="info_turnos">
                     <td>18:00</td>
-                    <td><div dia="0" horario="2" class="btn_turno"></td>
-                    <td><div dia="1" horario="2" class="btn_turno"></td>
-                    <td><div dia="2" horario="2" class="btn_turno"></td>
-                    <td><div dia="3" horario="2" class="btn_turno"></td>
-                    <td><div dia="4" horario="2" class="btn_turno"></td>
-                    <td><div dia="5" horario="2" class="btn_turno"></td>
-                    <td><div dia="6" horario="2" class="btn_turno"></td>
+                    <td><div dia="0" horario="2" class="turno_no_escogido"></td>
+                    <td><div dia="1" horario="2" class="turno_no_escogido"></td>
+                    <td><div dia="2" horario="2" class="turno_no_escogido"></td>
+                    <td><div dia="3" horario="2" class="turno_no_escogido"></td>
+                    <td><div dia="4" horario="2" class="turno_no_escogido"></td>
+                    <td><div dia="5" horario="2" class="turno_no_escogido"></td>
+                    <td><div dia="6" horario="2" class="turno_no_escogido"></td>
                 </tr>
             </table>
         </div>
     </div>
-    <script src="../assets/js/escoger-turno.js"></script>
-
+    <script src="./assets/js/escoger-turno.js"></script>
 </body>
 </html>
